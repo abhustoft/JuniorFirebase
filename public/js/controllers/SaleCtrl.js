98e8367f0ea11f13774e8ca64cc5f885a409edfb
Alf Bjørn <abhustoft@gmail.com>
2014-12-01 21:17:30 +0100

    Initialize Db for store and year

--- public/js/controllers/SaleCtrl.js
+++ public/js/controllers/SaleCtrl.js
@@ -4,35 +4,53 @@ angular.module('SaleCtrl', []).controller('SaleController', function(saleFactory
     this.tagline = 'The sales!';
     //this.sales = saleFactory.query();
     this.storeChoice = '';
+    this.janSales;
+    var list;
 
-    var year2014Ref = new Firebase("https://junioropen.firebaseio.com/Storo/2014/");
-    var janRef = year2014Ref.child('January');
-    var febRef = year2014Ref.child('February');
+    var initDB = function (store, year) {
 
-    var days = {};
+        var yearRef = new Firebase('https://junioropen.firebaseio.com/' + store + '/' + year + '/');
+        var days = {},
+            months = {};
 
-    for (var i = 0; i < 32; i++) {
-        days[i] = {"sale": 0};
+        for (var i = 0; i < 32; i++) {
+            days[i] = {"sale": 0, "temp": 23};
+        }
+
+        for (var i = 0; i < 13; i++) {
+            months[i] = days;
+        }
+        yearRef.set(months);
+
+
+
+        /******   *****/
+        var janRef = yearRef.child(1);
+        var sync = $firebase(janRef);
+        list = sync.$asArray();
+
+        list.$loaded().then(function () {
+            list.forEach(function (elem, index) {
+                console.log('Day: ' + index + ' ' + elem.sale + ' ' + elem.temp);
+            })
+        });
     }
 
-    janRef.set(days);
-    febRef.set(days);
+    initDB('Storo','2014');
+    initDB('Storo','2013');
+    initDB('Storo','2012');
+    initDB('Storo','2011');
+    initDB('Storo','2010');
 
-    var sync = $firebase(janRef);
-
-    var list = sync.$asArray();
-
-    list.$loaded().then(function() {
-
-        list.forEach(function (elem) {
-            console.log('Day: ' + elem.Day + ' ' + elem.Amount);
-        })
-    });
+    initDB('Sandvika','2014');
+    initDB('Sandvika','2013');
+    initDB('Sandvika','2012');
+    initDB('Sandvika','2011');
+    initDB('Sandvika','2010');
 
     this.janSales = list;
 
-
-
+    //this.sales =....
 
     this.getStoreSales = function () {
         var store = {'_store': this.storeChoice};
