de76c281716c2c6e7190bda6ddd72420547e3633
Alf Bjørn Hustoft <alfbjorn.hustoft@pearson.com>
2014-12-30 17:39:00 +0100

    Finished csvToDB.js

--- backend/csvToDB.js
+++ backend/csvToDB.js
@@ -4,75 +4,57 @@ var fs=require("fs");
 var Firebase       = require("firebase");
 var salesRef = new Firebase('https://junioropen.firebaseio.com/Sales');
 
-var param={};
+// Dummy variable
+var param = {};
 
-var files;
-var data;
-
-var rStream = [];
+var rStream      = [];
 var csvConverter = [];
-var csvFileName = [];
-var monthData = [];
+var csvFileName  = [];
 var convertCount = 0;
-var noOfFiles = 0;
+var noOfFiles    = 0;
 
 var dir = '/Users/abh/Projects/JuniorFirebase/juniorsales/';
 
-var options = {
-    cwd: process.cwd()
-};
-
-function rootFileName(fileName) {
-    return fileName.slice(0, fileName.lastIndexOf("."));
+/**
+ * Push JSON object, a day's sale, to Firebase
+ * @param jsonObj
+ */
+function toDB(jsonObj){
+    for (var c = 0; c < jsonObj.length; c++) {
+        salesRef.push(jsonObj[c]);
+    }
+    console.log('Converted: ' + csvFileName[convertCount]);
+    convertCount++;
 }
 
 /**
- *
- * @param err
- * @param stdout
- * @param stderr
+ * Register a format converter on each CSV file
+ * @param {array} files
  */
-function parseFiles (files){
-
+function registerFiles (files){
     noOfFiles = files.length;
+    for (var fn = 0; fn < files.length; fn++) {
 
-    for (var fn = 0; fn < noOfFiles; fn++) {
-
-        csvFileName[fn] = dir + 'csv/' + files[fn];
-        console.log('Registered csv File name: ' + csvFileName[fn]);
-
+        csvFileName[fn]  = dir + 'csv/' + files[fn];
         csvConverter[fn] = new Converter(param);
-
-        function convert(jsonObj){
-            console.log(jsonObj[0]); //here is your result csv object
-            var fd = fs.openSync(dir + 'json/' + rootFileName(files[convertCount]) + '.json','w');
-            var str = JSON.stringify(jsonObj);
-            fs.writeSync(fd,str);
-            monthData[convertCount] = jsonObj;
-            salesRef.push(jsonObj); //Decompose????
-            convertCount++;
-        }
+        rStream[fn]      = fs.createReadStream(csvFileName[fn]);
 
         //end_parsed will be emitted once parsing finished
-        csvConverter[fn].on("end_parsed", convert);
-
-        rStream[fn]  = fs.createReadStream(csvFileName[fn]);
+        csvConverter[fn].on("end_parsed", toDB);
     }
 }
 
-var files = fs.readdirSync(dir + 'csv/');
-parseFiles(files);
-
-//console.log(files);
-
+/**
+ * Activate a conversion stream
+ */
 function runStream () {
-    console.log('Runstream: ' + convertCount);
-    console.log('The csvsFileNames: ' + csvFileName)
     if (convertCount < noOfFiles) {
         rStream[convertCount].pipe(csvConverter[convertCount]);
     } else {
         clearInterval(timer);
+        console.log('Finished!');
     }
 }
 
-var timer = setInterval(runStream, 500)
+registerFiles(fs.readdirSync(dir + 'csv/'));
+var timer = setInterval(runStream, 1);
